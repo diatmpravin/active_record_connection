1618fab491677cf4e3695cff1f311d6c3c4027df
Pravin  Mishra <diatm.pravin.it.07.27@gmail.com>
2012-07-20 07:17:09 +0530

    moved database connection

--- lib/main.rb
+++ lib/main.rb
@@ -1,4 +1,23 @@
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
 
-puts "Hello World"
+require 'rubygems'
+require 'active_record'
+require 'yaml'
+
+dbconfig = YAML::load(File.open('db_connection.yml'))
+ActiveRecord::Base.establish_connection(dbconfig)
+
+class Location < ActiveRecord::Base
+
+  def initialize
+    p 'Tables details'
+    p Location
+    
+    p 'Count no of rows in table'
+    p Location.count
+  end
+
+end
+
+Location.new
